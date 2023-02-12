41031bbbcd32521b907f3403e78f43b36457b62d
Vitalii Vatulia <vitalii.vatulia@spryker.com>
2023-02-12 14:49:45 +0100

    Added Mercure lib https://localhost:2019/

--- infra/local/main.tf
+++ infra/local/main.tf
@@ -18,12 +18,18 @@ resource "null_resource" "docker_build" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "( cd ./../../ && docker build -t sudoku_php -f ./infra/docker/php/Dockerfile ./ )"
+    command = "( cd ./../../ && docker build -t sudoku_php -f ./infra/docker/php/Dockerfile ./ ) && ( cd ./../../ && docker build -t sudoku_mercure -f ./infra/docker/mercure/Dockerfile ./ )"
   }
 }
 
 data docker_image "sudoku_php" {
   name = "sudoku_php"
+
+  depends_on = [null_resource.docker_build]
+}
+
+data docker_image "sudoku_mercure" {
+  name = "sudoku_mercure"
 
   depends_on = [null_resource.docker_build]
 }
@@ -42,6 +48,23 @@ resource "docker_container" "php" {
   ports {
     internal = 80
     external = 80
+  }
+
+  network_mode = "bridge"
+  networks_advanced {
+    name = "sudoku_network"
+  }
+}
+
+resource "docker_container" "mercure" {
+  image = data.docker_image.sudoku_mercure.id
+  name  = "sudoku_mercure"
+
+  depends_on = [data.docker_image.sudoku_mercure]
+
+  ports {
+    internal = 2019
+    external = 2019
   }
 
   network_mode = "bridge"

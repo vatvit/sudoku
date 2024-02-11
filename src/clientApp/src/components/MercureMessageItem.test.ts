27e408742f7865ab03706aad56a03281a451f167
Vitalii Vatulia <vitalii.vatulia@spryker.com>
2024-02-11 18:52:17 +0100

    Added Vitest library and the first simple test

---
+++ src/clientApp/src/components/MercureMessageItem.test.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils'
+import MercureMessageItem from './MercureMessageItem.vue'
+
+test('mount component', async () => {
+    expect(MercureMessageItem).toBeTruthy()
+
+    const wrapper = mount(MercureMessageItem, {
+        props: {
+            id: 'someID',
+            data: {
+                key: 'someKey',
+            }
+        }
+    })
+
+    console.log(wrapper.text());
+
+    expect(wrapper.text()).toContain('ID: someID')
+    expect(wrapper.text()).toContain('"key": "someKey"')
+    expect(wrapper.text()).toMatchSnapshot()
+})
